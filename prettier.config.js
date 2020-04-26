c2b0b6d12d31dae0d4d4cd21f05123cb732b0e24
yago-codev <yagomilano92@gmail.com>
2020-04-26 20:18:52 -0300

    Adicionando Prettier

---
+++ prettier.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  singleQuote: true, // aspas simples
+  trailingComma: 'all', // inserindo vírgula no último item de arrays e objetos
+  arrowParams: 'avoid', // não colocar parênteses por volta de arrow functions quando tivermos somente 1 parâmetro
+};
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,8 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/', (request, response) => response.json({ mensagem: 'rota principal' }));
+routes.get('/', (request, response) =>
+  response.json({ mensagem: 'rota principal' }),
+);
 
 export default routes;
