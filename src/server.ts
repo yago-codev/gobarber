f437f8a50af8e70b4bcd2475f5a70fbd450a1e9d
yago-codev <yagomilano92@gmail.com>
2020-04-26 19:47:36 -0300

    Adicionando ESLint

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,9 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({ mensagem: "rota principal" });
-});
+app.get('/', (request, response) => response.json({ mensagem: 'rota principal' }));
 
 app.listen(3333, () => {
-  console.log("Servidor rodando na porta 3333");
+  console.log('Servidor rodando na porta 3333');
 });
